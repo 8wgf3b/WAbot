7a90f53bd1683c8610e31e1aa002fe375225656c
8wgf3b <karnabalaj@gmail.com>
2019-06-05 11:35:51 +0530

    Added sudoku function (unfinished) to test working with cv2

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
 from reddit import topretriever
-from media import echoimage, clean
+from media import echoimage, clean, sudoku
 from twilio.rest import Client
 import os
 
@@ -12,20 +12,31 @@ def reply():
     number = request.form['From']
     num_media = int(request.values.get("NumMedia"))
     message_body = request.form['Body']
-#    response = message_body
     message_body = message_body.split()
+
     if message_body[0].lower() == '!echo':
         response = ' '.join(message_body[1:])
+
     elif message_body[0].lower() == '!rtop':
         response = topretriever(message_body[1], message_body[2], int(message_body[3]), False)
+
     elif message_body[0].lower() == '!ecim' and num_media > 0:
         media_url = request.values.get(f'MediaUrl{0}')
         resp = MessagingResponse()
         resp.message(body = 'Echoed image').media(echoimage(media_url))
         clean()
         return str(resp)
+
+    elif message_body[0].lower() == '!sdku' and num_media > 0:
+        media_url = request.values.get(f'MediaUrl{0}')
+        resp = MessagingResponse()
+        resp.message(body = 'sudoku').media(sudoku(media_url))
+        clean()
+        return str(resp)
+
     else:
         response = '*Bruh Moment*'
+
     resp = MessagingResponse()
     resp.message(response)
     return str(resp)
--- media.py
+++ media.py
@@ -2,7 +2,7 @@ import pyimgur
 import requests
 import os, shutil
 import cv2
-import numpy
+import numpy as np
 
 def echoimage(URL):
     im = pyimgur.Imgur(os.environ['IMGUR_CID'])
@@ -24,3 +24,18 @@ def clean(path = 'temp/'):
                 os.unlink(file_path)
         except Exception as e:
             print(e)
+
+def sudoku(URL):
+        im = pyimgur.Imgur(os.environ['IMGUR_CID'])
+        #url can be sent directly but i wanted to test the temp directory's usage
+        response = requests.get(URL)
+        if response.status_code == 200:
+            with open("temp/sudoku.jpg", 'wb') as f:
+                f.write(response.content)
+        frame = cv2.imread('temp/sudoku.jpg')
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        frame = cv2.GaussianBlur(frame,(7, 7), 0)
+        frame = cv2.adaptiveThreshold(frame, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 2)
+        cv.imwrite('temp/solvedsudoku.jpg',frame)
+        uploaded_image = im.upload_image("temp/solvedsudoku.jpg", title="twilwhatbot")
+        return uploaded_image.link
