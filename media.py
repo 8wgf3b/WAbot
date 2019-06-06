139b08131ff904228aacd6016c55ad384f4fa44b
8wgf3b <karnabalaj@gmail.com>
2019-06-06 17:15:49 +0530

    initial commit for sudoku solver

--- media.py
+++ media.py
@@ -26,16 +26,15 @@ def clean(path = 'temp/'):
             print(e)
 
 def sudoku(URL):
-        im = pyimgur.Imgur(os.environ['IMGUR_CID'])
-        #url can be sent directly but i wanted to test the temp directory's usage
-        response = requests.get(URL)
-        if response.status_code == 200:
-            with open("temp/sudoku.jpg", 'wb') as f:
-                f.write(response.content)
-        frame = cv2.imread('temp/sudoku.jpg')
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        frame = cv2.GaussianBlur(frame,(7, 7), 0)
-        frame = cv2.adaptiveThreshold(frame, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 2)
-        cv2.imwrite('temp/solvedsudoku.jpg',frame)
-        uploaded_image = im.upload_image("temp/solvedsudoku.jpg", title="twilwhatbot")
-        return uploaded_image.link
+    im = pyimgur.Imgur(os.environ['IMGUR_CID'])
+    response = requests.get(URL)
+    if response.status_code == 200:
+        with open("temp/sudoku.jpg", 'wb') as f:
+            f.write(response.content)
+    frame = cv2.imread('temp/sudoku.jpg')
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    frame = cv2.GaussianBlur(frame,(7, 7), 0)
+    frame = cv2.adaptiveThreshold(frame, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 2)
+    cv2.imwrite('temp/solvedsudoku.jpg',frame)
+    uploaded_image = im.upload_image("temp/solvedsudoku.jpg", title="twilwhatbot")
+    return uploaded_image.link
