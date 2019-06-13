0326899a017fe41ad891ea059fb1f1bbfa214836
8wgf3b <karnabalaj@gmail.com>
2019-06-13 09:16:52 +0530

    Testing telegram bot

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, request
+import flask
+
 from twilio.twiml.messaging_response import MessagingResponse
 from reddit import topretriever, randomimageretriever
 from media import echoimage, clean, sudoku
@@ -7,7 +9,36 @@ from random import random
 from sports import getmatches, rawreturn
 import os
 
+
 app = Flask(__name__)
+website = os.environ['WEBSITE']
+token = os.environ['TEL_ACC_TOK']
+
+bot = telebot.TeleBot(API_TOKEN)
+
+
+
+@app.route('/'+token, methods=['POST'])
+def webhook():
+    if request.headers.get('content-type') == 'application/json':
+        json_string = request.get_data().decode('utf-8')
+        update = telebot.types.Update.de_json(json_string)
+        bot.process_new_updates([update])
+        return ''
+    else:
+        flask.abort(403)
+
+@bot.message_handler(commands=['help', 'start'])
+def send_welcome(message):
+    bot.reply_to(message,
+                 ("Hi there, I am EchoBot.\n"
+                  "I am here to echo your kind words back to you."))
+
+
+# Handle all other messages
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def echo_message(message):
+    bot.reply_to(message, message.text)
 
 @app.route('/inb', methods=['GET', 'POST'])
 def reply():
@@ -86,4 +117,6 @@ def reply():
 
 if __name__ == '__main__':
 #    startscheduling()
+    bot.remove_webhook()
+    bot.set_webhook(website+token)
     app.run()
