25989817c85e96df408d0d9edb9eac2e99f4c7ec
8wgf3b <karnabalaj@gmail.com>
2019-09-16 00:58:25 +0530

    minor bug fix

--- app.py
+++ app.py
@@ -161,7 +161,7 @@ def telegram():
 
                 else:
                     user = message_body[1].split('/')[-1]
-                    media_url, m1, m2 = relation(message_body[1:])['result']
+                    media_url, m1, m2 = relation('/ruseran', message_body[1:])['result']
                     mess = user + '\npost karma: '.ljust(20) + m1 + '\ncomment karma: '.ljust(20) + m2 + '\n'
                     payload = {'chat_id': chat_id, 'caption': mess, 'photo':media_url}
                     r = requests.post(telweb+token+'/'+'sendPhoto', json=payload)
@@ -173,7 +173,7 @@ def telegram():
                     response = 'example: /rsuban <subredditname>'
                     payload = {'chat_id': chat_id, 'text': response}
                 else:
-                    media_url, rank = relation(message_body[1:])['result']
+                    media_url, rank = relation('/rsuban', message_body[1:])['result']
                     mess = message_body[1] + '\n\n' + 'trending rank: ' + rank
                     payload = {'chat_id': chat_id, 'caption': mess, 'photo':media_url}
                     r = requests.post(telweb+token+'/'+'sendPhoto', json=payload)
@@ -255,7 +255,7 @@ def whatsapp():
 
     elif message_body[0].lower() == '/ruseran':
         resp = MessagingResponse()
-        media_url, m1, m2 = relation(message_body[1:])['result']
+        media_url, m1, m2 = relation('/ruseran', message_body[1:])['result']
         mess = 'post karma: '.ljust(20) + m1 + '\ncomment karma: '.ljust(20) + m2 + '\n'
         resp.message(body = mess).media(media_url)
         clean()
@@ -263,7 +263,7 @@ def whatsapp():
 
     elif message_body[0].lower() == '/rsuban':
         resp = MessagingResponse()
-        media_url, rank = relation(message_body[1:])['result']
+        media_url, rank = relation('/rsuban', message_body[1:])['result']
         mess = message_body[1] + '\n\n' + 'trending rank: ' + rank
         resp.message(body = mess).media(media_url)
         clean()
